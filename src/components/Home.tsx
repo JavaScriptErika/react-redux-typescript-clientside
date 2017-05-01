89ddba293155b5b17cc78c4fabf0325d1a978baf
levsthings <bearnode@gmail.com>
2017-05-01 15:52:40 +0300

    work on basics

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export class Home extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className='app'>
+              <h1>Hello via React!!!</h1>
+              <p></p>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.ts
@@ -0,0 +1,54 @@
+import * as path from 'path'
+import * as ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+export default {
+  entry: path.resolve(__dirname, 'src/webpackEntry.ts'),
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public'),
+    publicPath: '/',
+  },
+
+  devtool: 'cheap-module-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+          },
+        }, {
+          loader: 'ts-loader',
+        }],
+      },
+
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'sass-loader',
+          ],
+        }),
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'css/styles.css',
+      disable: false,
+      allChunks: true,
+    }),
+],
+}
