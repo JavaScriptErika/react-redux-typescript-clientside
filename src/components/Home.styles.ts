9643c3540de16124c0fa56e84e2f50da71ae2067
levsthings <bearnode@gmail.com>
2017-05-04 15:53:14 +0300

    First iteration

---
+++ src/components/Home.styles.ts
@@ -0,0 +1,51 @@
+import {StyleSheet} from 'aphrodite/no-important'
+
+const globalFont = {
+    fontFamily: 'Roboto-Regular',
+    fontStyle: 'normal',
+    src: "url('fonts/Roboto-Regular.ttf') format('ttf')"
+}
+
+export const styles = StyleSheet.create({
+    navBar: {
+        backgroundColor: 'black',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        height: '8vh'
+    },
+    textLogo: {
+        alignSelf: 'center',
+        color: '#fff',
+        fontFamily: [globalFont, 'sans-serif'],
+        fontSize: '1.2rem',
+        display: 'inline-block'
+    },
+    iconMenu: {
+        maxWidth: '20px',
+        display: 'inline-block'
+    },
+    iconSignIn: {
+        maxWidth: '20px',
+        display: 'inline-block'
+    },
+    wrapper: {
+        backgroundColor: 'white'
+    },
+    homepage: {
+        height: '80vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    searchContainer: {
+        fontFamily: [globalFont, 'sans-serif'],
+        fontSize: '1rem',
+        alignSelf: 'center',
+        textAlign: 'center'
+    },
+    search: {
+        minHeight: '3rem',
+        width: '90%'
+    }
+})
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,55 +1,27 @@
 import * as React from 'react'
-import {StyleSheet, css} from 'aphrodite/no-important'
-
+import { css } from 'aphrodite/no-important'
+import { styles } from './Home.styles'
 
 export class Home extends React.Component<{}, {}> {
     render() {
         return (
-                <div>
+            <div className={css(styles.wrapper)}>
                 <div className={css(styles.navBar)}>
-                    <img className={css(styles.menu)} src='assets/menu.svg'/>
-                    <h1 className={css(styles.logo)}>HOLLOWVERSE</h1>
-                    <img className={css(styles.signIn)} src='assets/sign-in.svg'/>
+                    <img className={css(styles.iconMenu)} src='assets/menu.svg' />
+                    <h1 className={css(styles.textLogo)}>XVERSE</h1>
+                    <img className={css(styles.iconSignIn)} src='assets/sign-in.svg' />
                 </div>
-                <div>
-                    <div>
+                <div className={css(styles.homepage)}>
+                    <div className={css(styles.searchContainer)}>
                         <h2>Search for a notable person.</h2>
+                        <input
+                            className={css(styles.search)}
+                            type='text'
+                            placeholder='Michael Jackson'
+                        />
                     </div>
                 </div>
-                </div>
+            </div>
         )
     }
 }
-
-const globalFont = {
-    fontFamily: 'Roboto-Regular',
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    src: "url('fonts/Roboto-Regular.ttf') format('ttf')"
-}
-
-const styles = StyleSheet.create({
-    navBar: {
-        backgroundColor: 'black',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        height: '5vh',
-        width: '100%'
-    },
-    logo: {
-        alignSelf: 'center',
-        color: '#fff',
-        fontFamily: [globalFont, 'sans-serif'],
-        fontSize: '1.2rem',
-        display: 'inline-block'
-    },
-    menu: {
-        maxWidth: '20px',
-        display: 'inline-block'
-    },
-    signIn: {
-        maxWidth: '20px',
-        display: 'inline-block'
-    }
-})
